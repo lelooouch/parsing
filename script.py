21d11d956ff9b682336b9fb81560be05bcbc2ca6
Maria Bashurova <xxxtacionxxx3@gmail.com>
2025-09-21 00:00:29 +0500

    берем цитату с сайта

--- script.py
+++ script.py
@@ -1,2 +1,10 @@
-print("hello world")
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://quotes.toscrape.com/"
+response = requests.get(url) # получаем ответ от сайта
+# print(response.text) - сырой текст html страницы
+soup = BeautifulSoup(response.text, "lxml") # lxml - анализатор html кода
+data = soup.find("div", class_="quote")
+name = data.find("span", class_="text")
+print(name)
