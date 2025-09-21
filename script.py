61655a6b4af8c9c66db93cd71e66f81e2f625778
Maria Bashurova <xxxtacionxxx3@gmail.com>
2025-09-21 19:22:32 +0500

    вывод описания автора

--- script.py
+++ script.py
@@ -5,6 +5,8 @@ from time import sleep
 headers = {"User-Agent":
            "Mozilla/5.0 (Windows NT 1; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0 (.NET CLR 89.0)"}
 
+list_authors_url = []
+
 for j in range(5):
 
     sleep(3)
@@ -19,9 +21,16 @@ for j in range(5):
     data = soup.find_all("div", class_="quote")
 
     for i in data:
-        quote_1 = i.find("span", class_="text").text
-        author_1 = i.find("small", class_="author").text.replace('\n', '')
-        url_author = "https://quotes.toscrape.com" + i.find("a").get("href")
-        print(quote_1 + "\n" + author_1 + "\n" + url_author + "\n")
+        about_author = "https://quotes.toscrape.com" + i.find("a").get("href")
+        list_authors_url.append(about_author)
+
+
+for ii in list_authors_url:
+    response = requests.get(ii, headers=headers)  # получаем ответ от сайта
+    soup = BeautifulSoup(response.text, "lxml")  # lxml - анализатор html кода
+    data = soup.find("div", class_="author-details")
+    desc = data.find("div", class_="author-description").text
+    print(desc)
+
 
 
