e32c12d3515086b7c58bb86752ea6c001282e741
Maria Bashurova <xxxtacionxxx3@gmail.com>
2025-09-21 13:43:00 +0500

    выводим все цитаты вместе с ссылками и авторами

--- script.py
+++ script.py
@@ -5,7 +5,12 @@ url = "https://quotes.toscrape.com/"
 response = requests.get(url) # получаем ответ от сайта
 # print(response.text) - сырой текст html страницы
 soup = BeautifulSoup(response.text, "lxml") # lxml - анализатор html кода
-data = soup.find("div", class_="quote")
-quote_1 = data.find("span", class_="text").text
-author_1 = data.find("small", class_="author").text.replace('\n', '')
-print(quote_1, author_1)
+data = soup.find_all("div", class_="quote")
+
+for i in data:
+    quote_1 = i.find("span", class_="text").text
+    author_1 = i.find("small", class_="author").text.replace('\n', '')
+    url_author = "https://quotes.toscrape.com" + i.find("a").get("href")
+    print(quote_1 + "\n" + author_1 + "\n" + url_author + "\n")
+
+
