15fbd5fc8f6a3521e53814b476d738eea67313f6
Maria Bashurova <xxxtacionxxx3@gmail.com>
2025-09-22 22:39:12 +0500

    оформляем базу

--- another_one.py
+++ another_one.py
@@ -6,8 +6,15 @@ def writer(parametr):
     book = xlsxwriter.Workbook(r"base1.xlsx")
     page = book.add_worksheet('цитата')
 
-    row = 0
+    page.write(0,0, 'цитата')
+    page.write(0, 1, 'автор')
+    page.write(0, 2, 'тэги')
+    page.write(0, 3, 'описание')
+
+    row = 1
     col = 0
+
+
 
     page.set_column('A:A', 20)
     page.set_column('B:B', 20)
@@ -15,7 +22,7 @@ def writer(parametr):
     page.set_column('D:D', 50)
 
     for item in parametr():
-        page.write(row, col, item[0])
+        page.write(row, col+0, item[0])
         page.write(row, col+1, item[1])
         page.write(row, col+2, item[2])
         page.write(row, col+3, item[3])
