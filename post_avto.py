4eb087bf1aaaab20580879377a59862192a97bae
Maria Bashurova <xxxtacionxxx3@gmail.com>
2025-09-28 20:00:14 +0500

    получаем токен 2.0

--- post_avto.py
+++ post_avto.py
@@ -8,6 +8,9 @@ response = work.get("https://quotes.toscrape.com/login")
 soup = BeautifulSoup(response.text, "lxml")
 
 token = soup.find("form").find("input").get("value")
+data = {"csrf_token": token, "username": "noname", "password": "12345"}
 
+result = work.post("https://quotes.toscrape.com/login", data=data, allow_redirects=True)
+print(result.text)
 
 
